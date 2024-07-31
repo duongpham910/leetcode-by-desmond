d79caf327f7eb067cc92695a366a8d9d447385bf
Desmond <duongpham91094@gmail.com>
2024-07-31 11:43:11 +0700

    Create next-greater-element-i.ts

---
+++ stack/next-greater-element-i.ts
@@ -0,0 +1,19 @@
+function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
+    let stack: number[] = []
+    let result: number[] = Array(nums1.length).fill(-1);
+    let obj = {}
+    for (let i = 0; i < nums1.length; i++) {
+        obj[nums1[i]] = i
+    }
+
+    for (let i = 0; i < nums2.length; i++) {
+        let cur = nums2[i]
+        while (stack.length != 0 && cur > stack[stack.length - 1]) {
+            let val = stack.pop()
+            result[obj[val]] = cur
+        }
+        if (nums1.includes(cur)) stack.push(cur)
+    }
+
+    return result
+};
