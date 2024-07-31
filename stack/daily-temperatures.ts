753e50b1f17f79bdaf463b4cf2400dfe898e7fd8
Desmond <duongpham91094@gmail.com>
2024-07-31 11:23:20 +0700

    Create daily-temperatures.ts

---
+++ stack/daily-temperatures.ts
@@ -0,0 +1,13 @@
+function dailyTemperatures(temperatures: number[]): number[] {
+    let stack: number[] = []
+    let result: number[] = Array(temperatures.length).fill(0);
+    for (let i = 0; i < temperatures.length; i++){
+        let cur = temperatures[i]
+        while(stack.length != 0 && cur > temperatures[stack[stack.length - 1]]) {
+            let index = stack.pop()
+            result[index] = i - index
+        }
+        stack.push(i)
+    }
+    return result
+};
