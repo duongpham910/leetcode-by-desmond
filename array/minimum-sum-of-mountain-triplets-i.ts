fef781e93a515036f81f0fb99a56bd6afa310eec
Desmond <duongpham91094@gmail.com>
2024-08-02 22:35:10 +0700

    Update minimum-sum-of-mountain-triplets-i.ts

--- array/minimum-sum-of-mountain-triplets-i.ts
+++ array/minimum-sum-of-mountain-triplets-i.ts
@@ -1,25 +1,20 @@
-function threeSum(nums: number[]): number[][] {
-    let final = []
-    nums = nums.sort((a,b) => a - b)
+function minimumSum(nums: number[]): number {
+    let min = 999999
 
     for (let i = 0; i < nums.length; i++) {
-        if (i > 0 && nums[i] == nums[i - 1]) continue
+        let cur = nums[i]
 
-        let l = i + 1
-        let r = nums.length - 1
-        while (l < r) {
-            if (nums[l] + nums[r] + nums[i] == 0) {
-                final.push([nums[i], nums[l], nums[r]])
-                l++
-                while(nums[l] == nums[l - 1] && l < r) l++
-            } else if (nums[l] + nums[r] + nums[i] > 0) {
+        for (let l = i + 1; l < nums.length; l++) {
+            let r = nums.length - 1
+            while (l < r) {
+                if (nums[i] < nums[l] && nums[r] < nums[l] && (nums[i] + nums[r] + nums[l] < min)) {
+                    min = nums[i] + nums[r] + nums[l]
+                }
                 r--
-            } else if (nums[l] + nums[r] + nums[i] < 0) {
-                l++
             }
-
         }
     }
 
-    return final
+    if (min == 999999) min = -1
+    return min
 };
